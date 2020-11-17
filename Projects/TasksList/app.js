c26a9c5a59eb9f48ee8a9e82cf8e2212f5476c70
simonet85 <christian.85@live.fr>
2020-11-17 18:48:37 +0100

    clear tasks added

--- Projects/TasksList/app.js
+++ Projects/TasksList/app.js
@@ -13,8 +13,39 @@ const taskInput = document.querySelector("#task")
 loadEventListener();
 //loading event
 function loadEventListener(){
+    document.addEventListener('DOMContentLoaded', getTasks)
     form.addEventListener('submit', addTask)
     taskslist.addEventListener('click', removeTask)
+    filter.addEventListener('keyup', filterTasks)
+    clearBtn.addEventListener('click', clearTasks)
+}
+
+//Get tasks from localStorage
+function getTasks(){
+    let tasks
+    if(localStorage.getItem("tasks") === null){
+        tasks = []
+    }else{
+        tasks.forEach(function(task){
+                // Create li element
+        const li = document.createElement('li');
+        // Add class
+        li.className = 'collection-item';
+        // Create text node and append to li
+        li.appendChild(document.createTextNode(task));
+        // Create new link element
+        const link = document.createElement('a');
+        // Add class
+        link.className = 'delete-item secondary-content';
+        // Add icon html
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        // Append the link to li
+        li.appendChild(link);
+
+        // Append li to ul
+        taskList.appendChild(li);
+        })
+    }
 }
 
 //Add Element
@@ -32,11 +63,14 @@ function addTask(e){
     a.innerHTML = "<i class='fa fa-remove'></i>"
     li.appendChild(a);
     taskslist.appendChild(li)  
+
+    //Save in localstorage
+    storeTaskInLocalStorage(taskInput.value)
+    //Clear input value
     taskInput.value = " "
 }
 
 //Remove Element
-
 function removeTask(e){
     e.preventDefault()
    
@@ -44,6 +78,57 @@ function removeTask(e){
        if( confirm("Do you really want to delete?")){
            e.target.parentElement.parentElement.remove()
        }
+    }
+}
+
+function filterTasks(e){
+    e.preventDefault()
+  
+  //Get the value of the input = term 
+  const searched = e.target.value.toLowerCase();
+  
+        let collection = document.querySelectorAll('.collection-item');
+       
+        collection.forEach(function(task){
+            //loop through the item collection
+            //Get Each firstChild element textContent of List Item
+            const item = task.firstChild.textContent
+
+            //Check if the index  of the searched term is in the list 
+            if(item.toLowerCase().indexOf(searched) != -1){
+                //If true display the element
+                task.style.display = "block"
+            }else{
+                //Else false display nothing
+                task.style.display = "none"
+            }
+        })
+}
+
+//Store task
+function storeTaskInLocalStorage( task ){
+    let tasks 
+    // Check out item in localstotage
+    if(localStorage.getItem("tasks") === null){
+        // Save an empty array
+        tasks = []
+    }else{
+        //Saving JSON format because localstorage stores strings only
+        tasks = JSON.parse(localStorage.setItem("tasks"))
+    }
+    tasks.push(task)
+
+    // Save tasks in string forrmat
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+// Clear Tasks
+function clearTasks() {
+    // taskList.innerHTML = '';
+  
+    // Faster
+    while(taskslist.firstChild) {
+      taskslist.removeChild(taskslist.firstChild);
     }
 }
     
