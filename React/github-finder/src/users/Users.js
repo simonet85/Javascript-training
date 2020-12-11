02010ab795a98bdacf2be127e62e9ae3da1ae742
simonet85 <christian.85@live.fr>
2020-12-11 17:42:53 +0100

    environment variable added

--- React/github-finder/src/users/Users.js
+++ React/github-finder/src/users/Users.js
@@ -1,41 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItems from './UserItems';
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        url: 'https://api.github.com/users/mojombo',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        id: 2,
-        login: 'defunkt',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        url: 'https://api.github.com/users/defunkt',
-        html_url: 'https://github.com/defunkt',
-      },
-      {
-        id: 3,
-        login: 'pjhyett',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        url: 'https://api.github.com/users/pjhyett',
-        html_url: 'https://github.com/pjhyett',
-      },
-    ],
-  };
-  render() {
+import PropTypes from 'prop-types';
+import Spinner from './../components/layouts/Spinner';
+
+const Users = ({ users, loading }) => {
+  // Loading the spinner
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={usersStyle}>
-        {this.state.users.map((user) => (
+        {users.map((user) => (
           <UserItems key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 const usersStyle = {
   display: 'grid',
