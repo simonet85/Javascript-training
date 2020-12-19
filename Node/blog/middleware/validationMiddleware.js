8d31a66c0998942dfa5c4b674c4e9dc9399b5821
simonet85 <christian.85@live.fr>
2020-12-19 16:21:59 +0100

    MVC Pattern added

---
+++ Node/blog/middleware/validationMiddleware.js
@@ -0,0 +1,8 @@
+
+//Custom MiddleWare
+module.exports = (req, res, next) => {
+    if (req.files == null || req.body.title == null || req.body.body == null) {
+      return res.redirect('/posts/new');
+    }
+    next();
+  };
