d37dda7d65c2691bea950b0dc05f6aaceee3f14d
simonet85 <christian.85@live.fr>
2020-12-19 17:33:42 +0100

    user register form completed

--- Node/blog/controllers/storeUser.js
+++ Node/blog/controllers/storeUser.js
@@ -4,6 +4,10 @@ const User = require('../models/User.js');
 
 module.exports = (req, res) => {
   User.create(req.body, (error, user) => {
+    // console.log(error);
+    if (error) {
+      return res.redirect('/auth/register');
+    }
     res.redirect('/');
   });
 };
