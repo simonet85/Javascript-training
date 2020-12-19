2e6fe38a05e06ffcfbfecc2a5d313ec36890bd0e
simonet85 <christian.85@live.fr>
2020-12-19 11:58:18 +0100

    Add fields the the schema

--- Node/blog/models/BlogPost.js
+++ Node/blog/models/BlogPost.js
@@ -4,6 +4,11 @@ const Schema = mongoose.Schema;
 const BlogPostSchema = new Schema({
   title: String,
   body: String,
+  username: String,
+  datePosted: {
+    type: Date,
+    default: new Date(),
+  },
 });
 
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
