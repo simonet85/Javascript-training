cf9b54971cfd92999683ba49dd1b484a561db99b
simonet85 <christian.85@live.fr>
2020-12-19 17:20:19 +0100

    encryption librarie added

--- Node/blog/models/User.js
+++ Node/blog/models/User.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   username: String,
   password: String,
+});
+
+UserSchema.pre('save', function (next) {
+  const user = this;
+
+  bcrypt.hash(user.password, 10, (error, hash) => {
+    user.password = hash;
+    next();
+  });
 });
 
 const User = mongoose.model('User', UserSchema);
